8d02f698574bdc602f9d6a4572fbcd854cd3f67f
zerostack <sahilmp8@gmail.com>
2015-07-24 14:12:43 +0000

    first commit

---
+++ main.R
@@ -0,0 +1,9 @@
+# San Francisco Crime CLassification
+# All analaysis will be drawn from a report.Rmd file
+
+# Load relevant libraryes
+# Report building
+library(knitr)
+
+knit2html("report.Rmd")
+
